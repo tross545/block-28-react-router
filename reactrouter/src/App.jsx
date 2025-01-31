64f25be96813bb30e2bd9fb180917c72eca5962c
Tristan Ross <Tristan@Tristans-MacBook-Pro-2.local>
2025-01-31 10:21:38 -0800

    Component files and links added. Color functionality completed

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -1,9 +1,26 @@
+import { Link, Routes, Route} from "react-router-dom";
+import Blue from "./components/Blue/Blue";
+import Red from "./components/Red/Red";
+import Home from "./components/Home/Home";
 
 function App() {
 
   return (
     <>
-      
+      <div id="container">
+        <div id="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+          <Route path="/" element={<Home />} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
